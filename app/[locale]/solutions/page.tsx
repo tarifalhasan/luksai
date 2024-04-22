cf1291f4e23aca0ff3efff740241cba6cc516e2a
tarifalhasan <tarifalhasan999@gmail.com>
2024-04-22 08:36:00 +0600

    SolutionsSession5

--- app/[locale]/solutions/page.tsx
+++ app/[locale]/solutions/page.tsx
@@ -1,6 +1,7 @@
 import SolutionSession2 from "@/components/pages/solutions/Session2";
 import SolutionsSession3 from "@/components/pages/solutions/Session3";
 import SolutionsSession4 from "@/components/pages/solutions/Session4";
+import SolutionsSession5 from "@/components/pages/solutions/Session5";
 import SolutionsHero from "@/components/pages/solutions/SolutionsHero";
 
 const page = () => {
@@ -10,6 +11,7 @@ const page = () => {
       <SolutionSession2 />
       <SolutionsSession3 />
       <SolutionsSession4 />
+      <SolutionsSession5 />
     </>
   );
 };
---
+++ components/pages/solutions/Session5.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { FaArrowRightLong } from "react-icons/fa6";
+
+const SolutionsSession5 = () => {
+  return (
+    <section className="  relative overflow-hidden py-10">
+      <div className="  relative container">
+        <div className=" grid lg:grid-cols-2 gap-5 lg:items-center">
+          <div>
+            <Image
+              src={"/images/solutions/session-5/artboard.svg"}
+              width={539}
+              height={403}
+              quality={80}
+              className=" w-full h-auto object-cover"
+              alt="background"
+            />
+          </div>
+          <div className=" space-y-4 lg:space-y-7">
+            <div>
+              <h4 className=" text-2xl font-bold text-skin-dark-900">
+                Engaging Content
+              </h4>
+            </div>
+            <p>
+              Deliver captivating material that keeps your audience hooked and
+              enhances learning outcomes. Our platform supports various formats,
+              including video, text, PDFs, downloads, external links, to cater
+              to diverse learning preferences and ensure a comprehensive
+              educational experience
+            </p>
+            <Button className=" gap-4">
+              Register Now <FaArrowRightLong />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SolutionsSession5;
